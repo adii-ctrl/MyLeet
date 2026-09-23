90f6b39c8a7e2224c3ca11241147313810c65b0d
Aditya Mourya <adityamourya192@gmail.com>
2026-09-23 20:35:59 +0530

    Time: 3 ms (76.34%), Space: 46.5 MB (60.94%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int romanToInt(String s) {
+        int ans =0;
+        int n = s.length();
+
+        for(int i =0;i< n;i++){
+            int curr = value(s.charAt(i));
+
+            if(i+1 < n && curr < value(s.charAt(i+1))){
+                ans-=curr;
+            }else{
+                ans+=curr;
+            }
+        }
+        return ans;
+    }
+    public int value(char c){
+        switch (c){
+
+            case 'I':return 1;
+            case 'V':return 5;
+            case 'X':return 10;
+            case 'L':return 50;
+            case 'C':return 100;
+            case 'D':return 500;
+            case 'M':return 1000;
+        }
+        return 0;
+    }
+}
